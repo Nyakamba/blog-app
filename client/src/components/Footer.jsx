5496701a336483ec384e0070d58b45d8c4f84fc6
Nyakamba <omwegaenock@gmail.com>
2024-05-21 17:58:18 +0300

    create and add the footer component

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,5 +1,26 @@
+import { Footer } from "flowbite-react";
+import { Link } from "react-router-dom";
+
 const FooterComp = () => {
-  return <div>Footer</div>;
+  return (
+    <Footer container className="border border-t-8 border-teal-500">
+      <div className="">
+        <div className="">
+          <div className="">
+            <Link
+              to="/"
+              className="self-center whitespace-nowrap text-lg sm:text-xl font-semibold dark:text-white"
+            >
+              <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500  to-pink-500 rounded-lg text-white">
+                Enock&apos;s
+              </span>
+              Blog
+            </Link>
+          </div>
+        </div>
+      </div>
+    </Footer>
+  );
 };
 
 export default FooterComp;
