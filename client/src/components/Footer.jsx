3c98ca46605b59c9c78ce887475cea004ce35205
Nyakamba <omwegaenock@gmail.com>
2024-05-28 22:15:53 +0300

    create footer component

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -25,7 +25,7 @@ const FooterComp = () => {
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                100 JS projects
+                100 JS projectss
               </Footer.Link>
             </Footer.LinkGroup>
           </div>
