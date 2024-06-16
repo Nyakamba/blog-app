4392273105fc6e6ef3b48050ec3081427ca34306
Nyakamba <omwegaenock@gmail.com>
2024-06-16 21:20:22 +0300

    create and add the footer component

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,12 +1,13 @@
 import { Footer } from "flowbite-react";
 import { Link } from "react-router-dom";
+import { BsFacebook, BsInstagram, BsTwitter, BsGithub } from "react-icons/bs";
 
 const FooterComp = () => {
   return (
     <Footer container className="border border-t-8 border-teal-500">
-      <div className="">
-        <div className="">
-          <div className="">
+      <div className="w-full max-w-7xl mx-auto">
+        <div className="grid w-full justify-between sm:flex md:grid-cols-1">
+          <div className="mt-5">
             <Link
               to="/"
               className="self-center whitespace-nowrap text-lg sm:text-xl font-semibold dark:text-white"
@@ -17,17 +18,63 @@ const FooterComp = () => {
               Blog
             </Link>
           </div>
-          <div className=" grid  grid-cols-2 gap-3 sm:mt-4 sm:grid-cols-3 sm:gap-6">
-            <Footer.Title title="About" />
-            <Footer.LinkGroup col>
-              <Footer.Link
-                href="https://www.100jsprojects.com"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                100 J project
-              </Footer.Link>
-            </Footer.LinkGroup>
+          <div className=" grid  grid-cols-2 gap-8 mt-4 sm:grid-cols-3 sm:gap-6">
+            <div>
+              <Footer.Title title="About" />
+              <Footer.LinkGroup col>
+                <Footer.Link
+                  href="https://www.100jsprojects.com"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  100 JS projects
+                </Footer.Link>
+                <Footer.Link
+                  href="/about"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Enock&apos;s Blog
+                </Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+            <div>
+              <Footer.Title title="Follow US" />
+              <Footer.LinkGroup col>
+                <Footer.Link
+                  href="https://www.github.com/Nyakamba"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Github
+                </Footer.Link>
+                <Footer.Link href="#">Discord</Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+            <div>
+              <Footer.Title title="Legal" />
+              <Footer.LinkGroup col>
+                <Footer.Link href="#">Privacy Policy</Footer.Link>
+                <Footer.Link href="#">Terms &amp; conditions</Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+          </div>
+        </div>
+        <Footer.Divider />
+        <div className="w-full sm:flex sm:items-center sm:justify-between">
+          <Footer.Copyright
+            href="#"
+            by="Enock's Blog"
+            year={new Date().getFullYear()}
+          />
+          <div className="flex gap-6 sm:mt-0 mt-4 sm:justify-center">
+            <Footer.Icon href="#" icon={BsFacebook} />
+            <Footer.Icon href="#" icon={BsTwitter} />
+            <Footer.Icon href="#" icon={BsInstagram} />
+            <Footer.Icon
+              href="https://www.github.com/Nyakamba"
+              icon={BsGithub}
+            />
           </div>
         </div>
       </div>
