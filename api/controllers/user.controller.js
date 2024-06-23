e7989a3d9599bc37974f501d56b5cc1b64af525d
Nyakamba <omwegaenock@gmail.com>
2024-06-23 09:40:31 +0300

    update user api

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -5,4 +5,7 @@ export const test = (req, res) => {
 export const updateUser = async (req, res, next) => {
   console.log(req.user);
   const { email, username, password } = req.body;
+  if (!email || !username || !password) {
+    next(401, "Please fill all the details");
+  }
 };
