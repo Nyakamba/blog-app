711b9a554452592c3dac56a39d7b07ddfd987409
Nyakamba <omwegaenock@gmail.com>
2024-06-23 22:15:38 +0300

    add update api user route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,10 +1,9 @@
 import express from "express";
-import { test, updateUser } from "../controllers/user.controller.js";
+import { updateUser } from "../controllers/user.controller.js";
 import { verfyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
 
-router.get("/test", test);
-router.put("/update:userId", verfyToken, updateUser);
+router.put("/update/:userId", verfyToken, updateUser);
 
 export default router;
