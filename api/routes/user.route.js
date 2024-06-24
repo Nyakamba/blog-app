2b4917f84bdf6c6ae08ab4aeb6eecc1be292a044
Nyakamba <omwegaenock@gmail.com>
2024-06-24 12:28:56 +0300

    delete user api route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { updateUser } from "../controllers/user.controller.js";
+import { deleteUser, updateUser } from "../controllers/user.controller.js";
+
 import { verfyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
 
 router.put("/update/:userId", verfyToken, updateUser);
+router.delete("/delete/:userId", verfyToken, deleteUser);
 
 export default router;
