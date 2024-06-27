b63fdc0365a603c39c0862249bbe83ed134dea64
Nyakamba <omwegaenock@gmail.com>
2024-06-27 23:03:19 +0300

    complete delete post api route

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,10 +1,15 @@
 import express from "express";
 import { verfyToken } from "../utils/verifyUser.js";
-import { create, getPosts } from "../controllers/post.controller.js";
+import {
+  create,
+  deletepost,
+  getPosts,
+} from "../controllers/post.controller.js";
 
 const router = express.Router();
 
 router.post("/create", verfyToken, create);
 router.get("/getposts", getPosts);
+router.delete("/deletepost/:postId/:userId", verfyToken, deletepost);
 
 export default router;
