8eb927e264747cbd36195465eb5d4f56af15ad37
Nyakamba <omwegaenock@gmail.com>
2024-06-27 15:49:21 +0300

    create posts api route

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { verfyToken } from "../utils/verifyUser.js";
-import { create } from "../controllers/post.controller.js";
+import { create, getPosts } from "../controllers/post.controller.js";
 
 const router = express.Router();
 
 router.post("/create", verfyToken, create);
+router.get("/getPosts", getPosts);
 
 export default router;
