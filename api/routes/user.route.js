177755af89ea37a5348046f53070c831a41e38cf
Nyakamba <omwegaenock@gmail.com>
2024-06-28 11:54:01 +0300

    create get users api route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,6 +1,7 @@
 import express from "express";
 import {
   deleteUser,
+  getUsers,
   signout,
   updateUser,
 } from "../controllers/user.controller.js";
@@ -12,5 +13,6 @@ const router = express.Router();
 router.put("/update/:userId", verfyToken, updateUser);
 router.delete("/delete/:userId", verfyToken, deleteUser);
 router.post("/signout", signout);
+router.get("/getusers", verfyToken, getUsers);
 
 export default router;
