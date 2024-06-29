d063bcfc77cd82f8cfad98957badb2b00b3e3c35
Nyakamba <omwegaenock@gmail.com>
2024-06-29 17:38:33 +0300

    add comment section to the posr page

--- api/routes/comment.route.js
+++ api/routes/comment.route.js
@@ -1,11 +1,7 @@
 import express from "express";
 import {
   createComment,
-  deleteComment,
-  editComment,
   getPostComments,
-  getcomments,
-  likeComment,
 } from "../controllers/comment.controller.js";
 import { verfyToken } from "../utils/verifyUser.js";
 
@@ -13,9 +9,9 @@ const router = express.Router();
 
 router.post("/create", verfyToken, createComment);
 router.get("/getPostComments/:postId", getPostComments);
-router.put("/likeComment/:commentId", verfyToken, likeComment);
-router.put("/editComment/:commentId", verfyToken, editComment);
-router.delete("/deleteComment/:commentId", verfyToken, deleteComment);
-router.get("/getcomments", verfyToken, getcomments);
+// router.put("/likeComment/:commentId", verfyToken, likeComment);
+// router.put("/editComment/:commentId", verfyToken, editComment);
+// router.delete("/deleteComment/:commentId", verfyToken, deleteComment);
+// router.get("/getcomments", verfyToken, getcomments);
 
 export default router;
