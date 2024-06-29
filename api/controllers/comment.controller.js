18e521d2002ccc6b74c82b846f401319822b59e6
Nyakamba <omwegaenock@gmail.com>
2024-06-29 16:59:14 +0300

    create comment api route

---
+++ api/controllers/comment.controller.js
@@ -0,0 +1,23 @@
+import { errorHandler } from "../utils/error";
+
+export const createComment = async (req, res, next) => {
+  try {
+    const { content, postId, userId } = req.body;
+    if (userId != req.user.id) {
+      return next(errorHandler("You are not allowed to create this comment"));
+    }
+
+    const newComment = new Comment({
+      content,
+      postId,
+      userId,
+    });
+    await newComment.save();
+
+    res.status(200).json(newComment);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getPostComments = async (req, res, next) => {};
