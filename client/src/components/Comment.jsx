085a93a6b094670926ef427d0c2b9d5e0dc5e690
Nyakamba <omwegaenock@gmail.com>
2024-06-30 11:37:39 +0300

    add delete funtionality to the comment component

--- client/src/components/Comment.jsx
+++ client/src/components/Comment.jsx
@@ -4,7 +4,7 @@ import { FaThumbsUp } from "react-icons/fa";
 import { useSelector } from "react-redux";
 import { Button, Textarea } from "flowbite-react";
 
-const Comment = ({ comment, onLike, onEdit }) => {
+const Comment = ({ comment, onLike, onEdit, onDelete }) => {
   const [user, setUser] = useState({});
   const [isEditing, setIsEditing] = useState(false);
   const [editedContent, setEditedContent] = useState(comment.content);
@@ -127,7 +127,7 @@ const Comment = ({ comment, onLike, onEdit }) => {
                     </button>
                     <button
                       type="button"
-                      //onClick={() => onDelete(comment._id)}
+                      onClick={() => onDelete(comment._id)}
                       className="text-gray-400 hover:text-red-500"
                     >
                       Delete
